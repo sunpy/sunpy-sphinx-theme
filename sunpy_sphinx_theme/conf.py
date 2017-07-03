05b92edbf858a4b182a829cef639921d318d2875
Stuart Mumford <stuart@cadair.com>
2017-07-03 17:29:58 +0100

    more nav bar links

--- sunpy_sphinx_theme/conf.py
+++ sunpy_sphinx_theme/conf.py
@@ -11,14 +11,18 @@ def page_url(page):
     sunpy_website_url_base = "https://duygukeskek.github.io/sunpy-website/"
     return urljoin(sunpy_website_url_base, page)
 
-html_theme_options = {
-    'navbar_links': [("About", page_url("about.html"), 1),
-                     ("Blog", page_url("blog.html"), 1),
-                     ("Docs", page_url("#")),
-                     ("Contribute", page_url("contribute.html"), 1),
-                     ("Affiliated Packages", page_url("affiliated.html"), 1),
-                     ("Help", page_url("help.html"), 1)
-                     ]}
 
-html_favicon = os.path.join(html_theme_path[0], html_theme, "static",
-                            "img", "favicon-32.ico")
+html_theme_options = {
+    'navbar_links': [
+        ("About", page_url("about.html"), 1),
+        ("Blog", page_url("blog.html"), 1),
+        ("Documentation", "#"),
+        ("Support Us", page_url("contribute.html"), 1),
+        ("Affiliated Projects", page_url("affiliated.html"), 1),
+        ("Get Help", page_url("help.html"), 1),
+        ("SunPy Project", page_url("team.html"), 1),
+    ]
+}
+
+html_favicon = os.path.join(html_theme_path[0], html_theme, "static", "img",
+                            "favicon-32.ico")
