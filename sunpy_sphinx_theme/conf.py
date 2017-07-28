b3c924e9c1850f12a124055b1c2adb4344ebdfe1
Nabil <nabil.freij@gmail.com>
2017-07-28 09:57:24 +0200

    Revert "url change"
    This reverts commit 1d5af28741b82b7183566209021ce6e07b10ba5e.

--- sunpy_sphinx_theme/conf.py
+++ sunpy_sphinx_theme/conf.py
@@ -7,7 +7,7 @@ html_theme_path = get_html_theme_path()
 html_theme = "sunpy"
 
 def page_url(page):
-    sunpy_website_url_base = "https://nabobalis.github.io/sunpy-website/"
+    sunpy_website_url_base = "https://duygukeskek.github.io/sunpy-website/"
     return urljoin(sunpy_website_url_base, page)
 
 
