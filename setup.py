1a6778f4a801e5b878af7b94225227eefe98d99a
Nabil <nabil.freij@gmail.com>
2017-08-15 19:48:58 +0200

    version 1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sunpy-sphinx-theme",
-    version="0.1.0",
+    version="1.0.0",
     use_2to3=False,
     description="The sphinx theme for the SunPy website and documentation.",
     long_description="",
