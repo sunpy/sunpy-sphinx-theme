1b48cab67f58b2cddd35598af2e666a28d2cb1e2
nabobalis <nabil.freij@gmail.com>
2018-04-13 16:32:37 +0200

    navbar reorder

--- sunpy_sphinx_theme/conf.py
+++ sunpy_sphinx_theme/conf.py
@@ -1,8 +1,9 @@
 import os
 try:
-    from urllib.parse import urljoin, urlparse
+    from urllib.parse import urljoin
 except ImportError:
-    from urlparse import urljoin, urlparse
+    from urlparse import urljoin
+import socket
 
 from sunpy_sphinx_theme import get_html_theme_path
 
@@ -11,16 +12,20 @@ html_theme = "sunpy"
 html_static_path = [os.path.join(html_theme_path[0], html_theme, "static")]
 html_favicon = os.path.join(html_static_path[0], "img", "favicon-32.ico")
 
+deploy = False
+if deploy:
+    sunpy_website_url_base = "http://sunpy.org/"
+else:
+    sunpy_website_url_base = socket.gethostname()
+
 
 def page_url(page):
-    sunpy_website_url_base = "http://sunpy.org/"
     return urljoin(sunpy_website_url_base, page)
 
 
 html_sidebars = {'**': ['docsidebar.html']}
 html_theme_options = {
-    'navbar_links': [
-        ("About", page_url("about.html"), 1),
+    'navbar_links': [  # About and documentation is hard coded due to drop down
         ("Blog", page_url("blog.html"), 1),
         ("Support Us", page_url("contribute.html"), 1),
         ("Get Help", page_url("help.html"), 1),
