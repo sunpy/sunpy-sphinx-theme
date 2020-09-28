38758448a78c4bf4274f0f80056645ae7d2741a1
Nabil Freij <nabil.freij@gmail.com>
2020-09-28 14:10:59 +0100

    Update conf.py

--- sunpy_sphinx_theme/conf.py
+++ sunpy_sphinx_theme/conf.py
@@ -10,8 +10,8 @@ html_static_path = [os.path.join(html_theme_path[0], html_theme, "static")]
 html_extra_path = [os.path.join(
     html_theme_path[0], html_theme, "static", "img")]
 templates_path = [os.path.join(html_theme_path[0], html_theme, "templates")]
-html_favicon = os.path.join(htmlstatic_path[0], "img", "favicon-32.ico")
-svg_icon = os.path.join(htmlstatic_path[0], "img", "sunpy_icon.svg")
+html_favicon = os.path.join(html_static_path[0], "img", "favicon-32.ico")
+svg_icon = os.path.join(html_static_path[0], "img", "sunpy_icon.svg")
 
 on_rtd = os.environ.get("READTHEDOCS", False) == "True"
 
