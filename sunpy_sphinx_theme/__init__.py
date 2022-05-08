a57d8d4a71aca3fb8c11bd45b55625069b3774c0
Nabil Freij <nabil.freij@gmail.com>
2022-05-07 17:44:38 -0700

    Tidy up as a base for the refactor (#142)

--- sunpy_sphinx_theme/__init__.py
+++ sunpy_sphinx_theme/__init__.py
@@ -1,14 +1,6 @@
 """
-SunPy Sphinx Theme.
+SunPy Sphinx Theme
+==================
+
+Based on the sphinx-bootstrap-theme.
 """
-import os
-
-import sphinx_bootstrap_theme
-
-
-def get_html_theme_path():
-    """
-    Return list of HTML theme paths.
-    """
-    cur_dir = os.path.abspath(os.path.dirname(__file__))
-    return [cur_dir] + sphinx_bootstrap_theme.get_html_theme_path()
---
+++ test_package/docs/api.rst
@@ -0,0 +1,5 @@
+***
+API
+***
+
+.. automodapi:: test_package
