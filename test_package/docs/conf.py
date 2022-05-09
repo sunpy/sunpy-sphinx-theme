a7ef9712d21115cbc8cb972e806bbc11f0ff8bc6
Nabil Freij <freij@baeri.org>
2022-05-09 12:50:35 -0700

    Revert "Tidy up as a base for the refactor (#142)"
    This reverts commit a57d8d4a71aca3fb8c11bd45b55625069b3774c0.

--- test_package/docs/conf.py
+++ test_package/docs/conf.py
@@ -1,12 +1,60 @@
 """
 Configuration file for the Sphinx documentation builder.
-"""
-import datetime
-import os
 
-project = "Test Package"
-author = "The Test Package Community"
+isort:skip_file
+"""
+# flake8: NOQA: E402
+
+# -- stdlib imports ------------------------------------------------------------
+import os
+import datetime
+
+
+# -- Read the Docs Specific Configuration --------------------------------------
+
+# This needs to be done before sunpy is imported
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+if on_rtd:
+    os.environ["SUNPY_CONFIGDIR"] = "/home/docs/"
+    os.environ["HOME"] = "/home/docs/"
+    os.environ["LANG"] = "C"
+    os.environ["LC_ALL"] = "C"
+
+# -- Project information -------------------------------------------------------
+
+project = "SunPy"
+author = "The SunPy Community"
 copyright = "{}, {}".format(datetime.datetime.now().year, author)
+
+# For the linkcheck
+linkcheck_ignore = [
+    r"https://doi.org/\d+",
+    r"https://riot.im/\d+",
+    r"https://github.com/\d+",
+    r"https://docs.sunpy.org/\d+",
+]
+linkcheck_anchors = False
+
+# This is added to the end of RST files - a good place to put substitutions to
+# be used globally.
+rst_epilog = """
+.. SunPy
+.. _SunPy: https://sunpy.org
+.. _`SunPy mailing list`: https://groups.google.com/group/sunpy
+.. _`SunPy dev mailing list`: https://groups.google.com/group/sunpy-dev
+"""
+
+# -- General configuration -----------------------------------------------------
+
+# Suppress warnings about overriding directives as we overload some of the
+# doctest extensions.
+suppress_warnings = [
+    "app.add_directive",
+]
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
     "matplotlib.sphinxext.plot_directive",
     "sphinx_automodapi.automodapi",
@@ -21,12 +69,38 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
+    "sunpy.util.sphinx.doctest",
+    "sunpy.util.sphinx.generate",
 ]
+
+# Add any paths that contain templates here, relative to this directory.
+# templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
 source_suffix = ".rst"
+
+# The master toctree document.
 master_doc = "index"
+
+# The reST default role (used for this markup: `text`) to use for all
+# documents. Set to the "smart" one.
 default_role = "obj"
+
+# Disable having a separate return type row
 napoleon_use_rtype = False
+
+# Disable google style docstrings
+napoleon_google_docstring = False
+
+# -- Options for intersphinx extension -----------------------------------------
+
+# Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     "python": (
         "https://docs.python.org/3/",
@@ -52,9 +126,23 @@ intersphinx_mapping = {
     "parfive": ("https://parfive.readthedocs.io/en/latest/", None),
     "reproject": ("https://reproject.readthedocs.io/en/stable/", None),
 }
+
+# -- Options for HTML output ---------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+
 from sunpy_sphinx_theme.conf import *  # NOQA
 
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']
+
+# Render inheritance diagrams in SVG
 graphviz_output_format = "svg"
+
 graphviz_dot_args = [
     "-Nfontsize=10",
     "-Nfontname=Helvetica Neue, Helvetica, Arial, sans-serif",
@@ -64,7 +152,9 @@ graphviz_dot_args = [
     "-Gfontname=Helvetica Neue, Helvetica, Arial, sans-serif",
 ]
 
-from sphinx_gallery.sorting import ExampleTitleSortKey, ExplicitOrder
+# -- Sphinx Gallery ------------------------------------------------------------
+from sphinx_gallery.sorting import ExplicitOrder  # NOQA
+from sphinx_gallery.sorting import ExampleTitleSortKey  # NOQA
 
 sphinx_gallery_conf = {
     "backreferences_dir": os.path.join("generated", "modules"),
