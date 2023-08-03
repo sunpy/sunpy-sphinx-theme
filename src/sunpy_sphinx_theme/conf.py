a70431d82e67f00698153c7c747b8d0f7753b431
Stuart Mumford <stuart@cadair.com>
2023-08-03 14:23:28 +0100

    Trying to fix ci

--- src/sunpy_sphinx_theme/conf.py
+++ src/sunpy_sphinx_theme/conf.py
@@ -1,1 +1,11 @@
+import os
+
+from sunpy_sphinx_theme import get_html_theme_path
+
 html_theme = "sunpy"
+
+html_theme_path = get_html_theme_path()
+html_static_path = [os.path.join(html_theme_path[0], html_theme, "static")]
+svg_icon = os.path.join(html_static_path[0], "img", "sunpy_icon.svg")
+png_icon = os.path.join(html_static_path[0], "img", "sunpy_icon_128x128.png")
+on_rtd = os.environ.get("READTHEDOCS", False) == "True"
