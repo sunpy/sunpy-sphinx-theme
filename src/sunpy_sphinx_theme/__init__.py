a3d47dfe28afedeef388620fa67805828abba615
Stuart Mumford <stuart@cadair.com>
2023-08-21 01:55:55 -0500

    Make the logo link to sunpy.org

--- src/sunpy_sphinx_theme/__init__.py
+++ src/sunpy_sphinx_theme/__init__.py
@@ -68,8 +68,11 @@ def update_config(app):
     # To do this, you must manually modify `app.builder.theme_options`.
     theme_options = utils.get_theme_options_dict(app)
 
+    if not theme_options.get("sst_site_root"):
+        theme_options["sst_site_root"] = "https://sunpy.org"
+
     if not theme_options.get("navbar_links"):
-        theme_options["navbar_links"] = default_navbar(theme_options["navbar_base"])
+        theme_options["navbar_links"] = default_navbar(theme_options["sst_site_root"])
 
     if not theme_options.get("footer_start"):
         theme_options["footer_start"] = ["page-footer.html"]
@@ -82,6 +85,13 @@ def update_config(app):
         ]
 
 
+def update_html_context(app: Sphinx, pagename: str, templatename: str, context, doctree) -> None:
+    """
+    Set extra things to use in jinja templates.
+    """
+    context["sst_site_root"] = app.builder.theme_options["sst_site_root"]
+
+
 def setup(app: Sphinx):
     # Register theme
     theme_dir = get_html_theme_path()
@@ -89,6 +99,7 @@ def setup(app: Sphinx):
     app.add_css_file("sunpy_style.css")
 
     app.connect("builder-inited", update_config)
+    app.connect("html-page-context", update_html_context)
 
     return {
         "parallel_read_safe": True,
