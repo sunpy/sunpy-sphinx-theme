1462aa14940f610be5b1a6f3caa0d70aa7fc2a20
Stuart Mumford <stuart@cadair.com>
2023-09-11 13:54:25 +0100

    Modify conf.py for backwards compat with drms (#225)

--- src/sunpy_sphinx_theme/conf.py
+++ src/sunpy_sphinx_theme/conf.py
@@ -6,8 +6,12 @@ of the theme.
 from sunpy_sphinx_theme import ON_RTD as on_rtd  # noqa
 from sunpy_sphinx_theme import PNG_ICON as png_icon  # noqa
 from sunpy_sphinx_theme import SVG_ICON as svg_icon  # noqa
+from sunpy_sphinx_theme import get_html_theme_path
 
-__all__ = ["html_theme", "on_rtd", "png_icon", "svg_icon"]
+__all__ = ["html_static_path", "html_theme_path", "html_theme", "on_rtd", "png_icon", "svg_icon"]
 
 html_theme = "sunpy"
 html_theme_options = {}
+
+html_theme_path = [str(get_html_theme_path())]
+html_static_path = [str(get_html_theme_path() / "static")]
