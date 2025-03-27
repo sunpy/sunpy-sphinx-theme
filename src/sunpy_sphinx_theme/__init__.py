e87458e4f3113ebfdd8cef6e87ce5143973fc2dc
hayesla <lauraannhayes@gmail.com>
2025-03-27 13:46:18 +0000

    adding citing sunpy

--- src/sunpy_sphinx_theme/__init__.py
+++ src/sunpy_sphinx_theme/__init__.py
@@ -58,6 +58,8 @@ def default_navbar():
         ("Get Help", "help/", 2),
         ("Contribute", "contribute/", 2),
         ("Blog", "blog/", 2),
+        ("Citing SunPy", "https://docs.sunpy.org/en/stable/citation.html", 3),
+
     ]
 
 
