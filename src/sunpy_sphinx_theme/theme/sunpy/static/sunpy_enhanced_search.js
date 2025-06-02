2cf5674b6438c4fb6bafb060be60d94c27ec2984
Stuart Mumford <stuart@cadair.com>
2025-06-02 16:38:11 +0100

    Use /_/ proxy on RTD

--- src/sunpy_sphinx_theme/theme/sunpy/static/sunpy_enhanced_search.js
+++ src/sunpy_sphinx_theme/theme/sunpy/static/sunpy_enhanced_search.js
@@ -348,8 +348,12 @@
 			form.classList.add('loading');
 
 			let projstr = this.projectorder.join('+project:').replace(new RegExp("^"+config.all+"[\s\+]"),'');
-			let url = (page || ("https://readthedocs.org/api/v3/search/?q=" + projstr + "+" + encodeURIComponent(str)));
-			if(debug) url = "https://corsproxy.io/?" + url;
+			let path = (page || ("api/v3/search/?q=" + projstr + "+" + encodeURIComponent(str)));
+			if(debug) {
+				url = "https://corsproxy.io/?https://readthedocs.org/" + path;
+			}else{
+				url = "/_/" + path;
+			};
 			console.info('Getting '+url);
 			fetch(url,{}).then(response =>{
 				return response.json();
