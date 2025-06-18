3882c00d2a5dc06b8bd157eb7c823487e9f0c53c
Stuart Lowe <299787+slowe@users.noreply.github.com>
2025-06-18 19:07:06 +0100

    Add filter icon

--- src/sunpy_sphinx_theme/theme/sunpy/static/sunpy_enhanced_search.js
+++ src/sunpy_sphinx_theme/theme/sunpy/static/sunpy_enhanced_search.js
@@ -58,6 +58,10 @@
 		this.results = {};
 		this.tabs = {};
 		this.selectedPanel = config.all;
+		let icons = {
+			'filter':'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5z"/></svg>',
+			'book':'<svg width="16" height="16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M96 0C43 0 0 43 0 96V416c0 53 43 96 96 96H384h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V384c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H384 96zm0 384H352v64H96c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16zm16 48H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16s7.2-16 16-16z"></path></svg>'
+		};
 
 		// Build the list of projects
 		this.projects = {};
@@ -276,7 +280,7 @@
 			let slug = this.projectorder[p];
 			if(!this.projects[slug].tab){
 				let tab = document.createElement('button');
-				tab.innerHTML = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="book" class="svg-inline--fa fa-book" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M96 0C43 0 0 43 0 96V416c0 53 43 96 96 96H384h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V384c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H384 96zm0 384H352v64H96c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16zm16 48H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16s7.2-16 16-16z"></path></svg> ' + this.projects[slug].name + '<span class="n"></span>';
+				tab.innerHTML = (slug==config.all ? icons.book : icons.filter) + ' ' + this.projects[slug].name + '<span class="n"></span>';
 				tab.id = "tab-" + slug;
 				tab.classList.add('tab');
 				tab.setAttribute('role','tab');
